3bf2c116d972e71bd69793b9832e5f84c2f5fa5d
Jeromekene <77003839+scelloo-elegeonyejerome@users.noreply.github.com>
2021-10-24 21:21:33 +0100

    initial commit

---
+++ src/messages/messages.services.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { MessageRepository } from './messages.repository';
+
+@Injectable()
+export class MessagesService {
+
+  constructor(public messagesRepo: MessageRepository) {}
+
+  async findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  async findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  async create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
